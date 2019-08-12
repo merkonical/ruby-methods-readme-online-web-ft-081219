933c4bd3849ef9e5cbc242004453be8ffeeb63b8
Maverick Adolfo <merkonical@gmail.com>
2019-08-12 16:16:34 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,20 @@
 # Use this space to code along with the readme
+def greeting 
+  puts "Hello World"
+end 
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+greeting
